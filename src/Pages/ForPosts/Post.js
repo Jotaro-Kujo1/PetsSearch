0ffd22a0aa5f7440f06abe90b0f8eb40ca1add19
Vadim <lebedev.vada@gmail.com>
2022-08-10 13:54:14 +0300

    сервер отрабатывает, но приходит null

--- src/Pages/ForPosts/Post.js
+++ src/Pages/ForPosts/Post.js
@@ -4,7 +4,7 @@ import {PhotoCamera} from "@mui/icons-material";
 import Button from "@mui/material/Button";
 import SendIcon from '@mui/icons-material/Send';
 import React, {useEffect, useState} from "react";
-
+import axios from 'axios';
 
 
 
@@ -15,24 +15,44 @@ export const Post = () => {
     const[description,setDescription] = useState('');
     const[address,setAddress] = useState('');
 
+
     useEffect(()=>{
         if(selectedImage) {
             setUrl(URL.createObjectURL(selectedImage));
         }
     },[selectedImage]);
 
+
+
     const handleClick = () => {
-        const newPost = {url,description,address,user_name};
-        console.log(newPost);
+        var img = [];
+        const newPost = {img,description,address,user_name};
         fetch("http://localhost:8080/posts/createPost",{
                 method:"POST",
                 headers:{"Content-Type":"application/json"},
                 body:JSON.stringify(newPost)
-            }
+        }
         ).then((response) => {
             console.log(response.status);
         })
     }
+
+    const test = () => {
+        let file = new FormData();
+        var img = [];
+        file.set("file",selectedImage);
+        fetch("http://localhost:8080/posts/byteConverter", {
+            mode: "no-cors",
+            method: "POST",
+            body: file
+        }).then((response) => {
+            console.log(response.status);
+            img = response.body;
+            console.log(img);
+        })
+    }
+
+
 
     return (
         <>
@@ -54,7 +74,7 @@ export const Post = () => {
                 <input hidden accept="image/*" type="file" onChange={e => setSelectedImage(e.target.files[0])}/>
                 <PhotoCamera/>
             </IconButton>
-            <Button variant="contained" className="publicBtn" startIcon={<SendIcon/>} onClick={handleClick}>Public post</Button>
+            <Button variant="contained" className="publicBtn" startIcon={<SendIcon/>} onClick={test}>Public post</Button>
             <img className="image" src={url} height="280px" width="230px"/>
         </>
     )
